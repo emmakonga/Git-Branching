dc014f0d652df8bf77971cc06c11dcc2a0ebe24f
Emmanuel <emmanymakonga@gmail.com>
2017-01-31 12:25:51 -0700

    Added comment to all my scripts

--- AddUnityProject_Git/Assets/Scripts/MyScript2.cs
+++ AddUnityProject_Git/Assets/Scripts/MyScript2.cs
@@ -5,11 +5,11 @@ public class MyScript2 : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		//I Could initialize Variable here
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		//I can update my variable here
 	}
 }
