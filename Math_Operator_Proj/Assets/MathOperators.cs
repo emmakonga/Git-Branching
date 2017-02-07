db4ca8ae4643ea79cb4a10bc503c3d2922a66c99
Emmanuel <emmanymakonga@gmail.com>
2017-02-07 12:33:51 -0700

    This are some examples of MathOperators' assignment.

--- Math_Operator_Proj/Assets/MathOperators.cs
+++ Math_Operator_Proj/Assets/MathOperators.cs
@@ -5,6 +5,7 @@ public class MathOperators : MonoBehaviour {
     public int a = 12;
     public int b = 2;
     public int c = 3;
+	public float d = 5f;
     public  int myInt;
     // Use this for initialization
     void Start()
@@ -21,14 +22,29 @@ public class MathOperators : MonoBehaviour {
         int modInt = a % b;
         //creates an int variable equal (a + b) * c 
         int anotherInt = (a + b) * c;
-        print(myInt);
-      
+		//creates an int variable equal to (b * c) + a
+		int mathInt = (b * c) + a;
+		//creates a Floating point variable equal to mathInt + divInt * subInt / c
+		float myFloat = mathInt + divInt * subInt / c;
+		//creates a floating variable equal to(A times B * c) / d
+		float myFloat2 = (a * b * c) / d;
+		//creates a floating variable equal to myInt + subInt - d
+		float myFloat3 = myInt + subInt - d;
+
+		//print my values
+		print(myInt);
         print(subInt);
         print(multInt);
         print(a * b);
+		//This line performs multiplication inside the print statement
         print(multInt * subInt);
         print(divInt);
         print(modInt);
         print(anotherInt);
+		print(mathInt);
+		print(myFloat);
+		print(myFloat2);
+		print(myFloat3);
     }
+
 }
