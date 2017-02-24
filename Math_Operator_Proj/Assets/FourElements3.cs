0b14069015ed2cab546406026e4d9e237e389dc1
Unknown <emmanymakonga@gmail.com>
2017-02-24 11:44:48 -0700

    4ElementsRedo

---
+++ Math_Operator_Proj/Assets/FourElements3.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FourElements3 : MonoBehaviour {
+
+	public List<string> animals;
+
+	// Use this for initialization
+	void Start ()
+	{
+		foreach (string item in animals)
+
+
+			if(item == "Cat")
+			{
+
+				print(item);
+			}
+
+
+		{
+
+		}
+	}
+
+}
