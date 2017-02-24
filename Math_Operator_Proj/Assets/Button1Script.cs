d2a903bf800a965408d4ba4b94226e99b4ee9f5b
Emmanuel <emmanymakonga@gmail.com>
2017-02-24 12:11:09 -0700

    Added Animal Buttons

---
+++ Math_Operator_Proj/Assets/Button1Script.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Button1Script : MonoBehaviour {
+
+	public List<string> animals;
+	string animal = "Cat";
+
+	// Use this for initialization
+	void Start () {
+		animals = new List<string>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void AddAnimal()
+	{
+		animals.Add (animal);
+	}
+}
