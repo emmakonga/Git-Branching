4fec8b5a0883f63d66c7b3226fb9acad26b48261
Unknown <emmanymakonga@gmail.com>
2017-02-24 11:11:25 -0700

    4Elements List

---
+++ Math_Operator_Proj/Assets/FourElementsList.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class FourElementsList : MonoBehaviour {
+
+    public List<string> animals;
+
+	// Use this for initialization
+	void Start ()
+    {
+    foreach (string item in animals)
+
+ 
+	    if(item == "Cat")
+    {
+
+         print(item);
+    }
+
+
+    {
+
+    }
+    }
+
+}
+
